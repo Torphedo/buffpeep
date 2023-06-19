f588c53cfd4ca395bab0a99633237481b433481b
Torphedo <torphedo.launcher@gmail.com>
2023-06-19 01:26:28 -0400

    Rename sampler

--- gl_box/src/gl/fragment.glsl
+++ gl_box/src/gl/fragment.glsl
@@ -4,8 +4,8 @@ out vec4 fragment_rgba;
 in vec3 frag_out;
 in vec2 tex_coord;
 
-uniform sampler2D sampler;
+uniform sampler2D perlin;
 
 void main() {
-  fragment_rgba = texture(sampler, tex_coord);
+  fragment_rgba = texture(perlin, tex_coord);
 }
