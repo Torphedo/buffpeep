226864af033f4cb5d0dc1778c6473593bf24fccf
Torphedo <torphedo.launcher@gmail.com>
2023-06-22 18:14:09 -0400

    Working orbit camera

--- gl_box/src/camera.c
+++ gl_box/src/camera.c
@@ -1,36 +1,45 @@
+#include <cglm/struct.h>
+#include <GLFW/glfw3.h>
+
+#include "logging.h"
 #include "camera.h"
-#include "GLFW/glfw3.h"
 
 vec3s camera_pos = {0};
 vec3s camera_target = {0};
-vec3 camera_up = {0.0f, 1.0f, 0.0f};
+
+vec3s camera_orbit_pos = {0};
+
+const float radius = 4.0f;
 
 void update_camera() {
-  camera_target = camera_pos;
-  camera_target.x += 0.1f;
+  camera_pos.x = sin(camera_orbit_pos.x) * radius;
+  camera_pos.z = cos(camera_orbit_pos.z) * radius;
+
+  // TODO: Vertical orbit camera is clunky
+  // This is a little weird/annoying to use, because it will only let you pan
+  // in a 90 degree vertical range... Find some way to make this work for at
+  // least 180 degrees.
+  camera_pos.y = sin(camera_orbit_pos.y) * radius;
+
+  // TODO: Implement flying freecam-style camera [low priority]
 }
 
-// If we want the camera movement to always be relative to the view (e.g. for a
-// freecam-style flying controller), then we'll need a "camera front" offset
-// for the camera to look at, then move along the local axes.
-//
-// But since we'll probably need a third-person orbiting camera, we need to add
-// a normalized camera position vector to the camera target, then just look at
-// the target. To rotate, we just add to the position vector and normalize it.
 void camera_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
   if (action == GLFW_PRESS || action == GLFW_REPEAT) {
     switch (key) {
       case GLFW_KEY_W:
-        camera_pos.x += 0.2f;
+        camera_orbit_pos.y += 0.1f;
         break;
       case GLFW_KEY_S:
-        camera_pos.x -= 0.2f;
+        camera_orbit_pos.y -= 0.1f;
         break;
       case GLFW_KEY_A:
-        camera_pos.z -= 0.2f;
+        camera_orbit_pos.x -= 0.1f;
+        camera_orbit_pos.z -= 0.1f;
         break;
       case GLFW_KEY_D:
-        camera_pos.z += 0.2f;
+        camera_orbit_pos.x += 0.1f;
+        camera_orbit_pos.z += 0.1f;
         break;
     }
   }
--- gl_box/src/camera.h
+++ gl_box/src/camera.h
@@ -4,7 +4,7 @@
 
 extern vec3s camera_pos;
 extern vec3s camera_target;
-extern vec3 camera_up;
+static const vec3 camera_up = {0.0f, 1.0f, 0.0f};
 
 void update_camera();
 void camera_key_callback(GLFWwindow* window, int key, int scancode, int actions, int mods);
