e4203bce7ff995c92c00757aeb79106566341bdc
torph <73564623+Torphedo@users.noreply.github.com>
2023-09-06 12:56:07 -0400

    Print message type in debug messages

--- gl_box/src/gl_debug.c
+++ gl_box/src/gl_debug.c
@@ -4,8 +4,11 @@
 
 #include "logging.h"
 
-void gl_debug_msg(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message, const void *userParam) {
+void APIENTRY gl_debug_msg(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message, const void *userParam) {
   char* source_str = NULL;
+  char* type_str = NULL;
+  char* severity_str = NULL;
+
   switch (source) {
       case GL_DEBUG_SOURCE_SHADER_COMPILER_ARB:
           source_str = "shader compiler";
@@ -29,19 +32,57 @@ void gl_debug_msg(GLenum source, GLenum type, unsigned int id, GLenum severity, 
           source_str = "[unknown source]";
   }
 
+  switch (type) {
+      case GL_DEBUG_TYPE_ERROR:
+          type_str = "ERROR";
+          break;
+      case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+          type_str = "DEPRECATED";
+          break;
+      case GL_DEBUG_TYPE_PERFORMANCE:
+          type_str = "PERFORMANCE";
+          break;
+      case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+          type_str = "UNDEFINED BEHAVIOUR";
+          break;
+      case GL_DEBUG_TYPE_PORTABILITY:
+          type_str = "PORTABILITY WARNING";
+          break;
+      case GL_DEBUG_TYPE_OTHER:
+          type_str = "OTHER";
+          break;
+      case GL_DEBUG_TYPE_PUSH_GROUP:
+          type_str = "PUSH GROUP";
+          break;
+      case GL_DEBUG_TYPE_POP_GROUP:
+          type_str = "POP GROUP";
+          break;
+      default:
+          type_str = "other type";
+  }
+
   switch (severity) {
       case GL_DEBUG_SEVERITY_HIGH_ARB:
-        LOG_MSG(error, "OpenGL %s message [CRITICAL] ", source_str);
+        LOG_MSG(error, "");
+        severity_str = "CRITICAL";
         break;
       case GL_DEBUG_SEVERITY_MEDIUM_ARB:
-        LOG_MSG(warning, "OpenGL %s message [WARNING] ", source_str);
+        LOG_MSG(warning, "");
+        severity_str = "WARNING";
         break;
       case GL_DEBUG_SEVERITY_LOW_ARB:
-        LOG_MSG(info, "OpenGL %s message [LOW] ", source_str);
+        LOG_MSG(info, "");
+        severity_str = "LOW";
+        break;
+      case GL_DEBUG_SEVERITY_NOTIFICATION:
+        LOG_MSG(debug, "");
+        severity_str = "NOTIFICATION";
         break;
       default:
-        LOG_MSG(debug, "OpenGL %s message [DEBUG] ", source_str);
+        LOG_MSG(debug, "");
+        severity_str = "DEBUG";
   }
+  printf("OpenGL %s message [%s] [%s] ", source_str, severity_str, type_str);
   printf("[id %d]: %s\n", id, message);
 }
 
@@ -56,9 +97,11 @@ void gl_debug_setup() {
         glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
         if (flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
             glEnable(GL_DEBUG_OUTPUT);
+            // Debug callbacks will be on the same thread
             glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
             glDebugMessageCallbackARB(gl_debug_msg, NULL);
-            // No message filtering, we print EVERYTHING.
+
+            // Don't filter out any messages
             glDebugMessageControlARB(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
         }
     }
