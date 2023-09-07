2a772165690b13fd804f071c08a0c3d0a1be7e9c
torph <73564623+Torphedo@users.noreply.github.com>
2023-09-07 10:47:25 -0400

    Only scale by the radius once

--- src/camera.c
+++ src/camera.c
@@ -43,17 +43,17 @@ void update_camera(mat4* view) {
   camera_orbit_angles.x += orbit_speed * input.d;
 
   // Rendering breaks at exactly 90 and we don't want to be upside-down
-  camera_orbit_angles.y = clampf(camera_orbit_angles.y, glm_rad(89.9999) * -1.0f, glm_rad(89.9999));
+  camera_orbit_angles.y = clampf(camera_orbit_angles.y, glm_rad(-89.9999f), glm_rad(89.9999f));
 
   // Get XYZ positions using trig on our angles
-  camera_pos.x = sin(camera_orbit_angles.x) * radius;
-  camera_pos.z = cos(camera_orbit_angles.x) * radius;
-  camera_pos.y = sin(camera_orbit_angles.y) * radius;
+  camera_pos.x = sin(camera_orbit_angles.x);
+  camera_pos.z = cos(camera_orbit_angles.x);
+  camera_pos.y = sin(camera_orbit_angles.y);
 
   // To keep the camera at a constant distance we need to move our horizontal
   // position vector towards (0, 0) by the distance between the radius and the
   // Z position of our Y angle.
-  float z_diff = (1.0f - cos(camera_orbit_angles.y)) * radius;
+  float z_diff = (1.0f - cos(camera_orbit_angles.y));
 
   // Make sure we're SHORTENING the vector, not adding to it.
   z_diff = fabsf(z_diff); 
@@ -64,6 +64,9 @@ void update_camera(mat4* view) {
 
   camera_pos.x = horizontal_pos.x;
   camera_pos.z = horizontal_pos.y;
+
+  // Scale our position to the orbit radius
+  camera_pos = glms_vec3_scale_as(camera_pos, radius);
 
   // TODO: Implement flying freecam-style camera [low priority]
   glm_lookat((float*)&camera_pos, (float*)&camera_target, camera_up, *view);
