10018b80a6fcc607055c0865c0a52e3c7f7fa38b
torph <73564623+Torphedo@users.noreply.github.com>
2024-03-03 21:10:51 -0500

    Fix GLSL header codegen script

--- gl_codegen.sh
+++ gl_codegen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sed '/^ *$/d;s/.*/"&\\\n"/' src/gl/vertex.glsl > src/gl/vertex.h
-sed '/^ *$/d;s/.*/"&\\\n"/' src/gl/fragment.glsl > src/gl/fragment.h
+# Some sed/regex black magic to put quotes at the beginning and end of every
+# line. Also adds "\n" before each closing quote, so the shaders compile.
+sed '/^ *$/d;s/.*/"&\\n"/' src/gl/vertex.glsl > src/gl/vertex.h
+sed '/^ *$/d;s/.*/"&\\n"/' src/gl/fragment.glsl > src/gl/fragment.h
 
